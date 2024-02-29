72c335f14913fcc74ba1bac638ca93ddd0bc5f87
okchichkina <olka.chichkina@gmail.com>
2024-02-29 15:29:09 +0300

    up

--- header_script.js
+++ header_script.js
@@ -1,39 +1,23 @@
 window.onscroll = function() { 
-    scrollFunction() 
+  scrollFunction() 
 }; 
-      
-function scrollFunction() { 
-    if (document.body.scrollTop > 50 || 
-        document.documentElement.scrollTop > 50) 
-    { 
-        document.getElementById("header") 
-                    .style.maxHeight = "80px";
-        document.getElementById("header") 
-                    .style.minHeight = "70px";          
-              
-        document.getElementById("logo_img") 
-                .style.maxWidth = "70px";
-        document.getElementById("logo_img") 
-                .style.minWidth = "60px"; 
-        document.getElementById("logo_img") 
-                .style.minHeight = "60px";
-        document.getElementById("logo_img") 
-                .style.maxHeight = "70px";
-    } 
-    else { 
-        document.getElementById("header") 
-                    .style.maxHeight = "177px";
-        document.getElementById("header") 
-                    .style.minHeight = "160px";    
-        document.getElementById("logo_img") 
-                .style.maxWidth = "140px"; 
-        document.getElementById("logo_img") 
-                .style.minWidth = "130px"; 
-        document.getElementById("logo_img") 
-                .style.minHeight = "130px";
-
-        document.getElementById("logo_img") 
-                .style.maxHeight = "140px";
-    } 
-} 
     
+function scrollFunction() { 
+  if (document.body.scrollTop > 0 || 
+      document.documentElement.scrollTop > 0) 
+  { 
+      document.getElementById("header") 
+                  .style.height = "80px";
+      document.getElementById("tytle") 
+                  .style.whiteSpace = "nowrap";
+      }
+  
+  else { 
+      document.getElementById("header") 
+                  .style.height = "30vh";
+      document.getElementById("tytle") 
+                  .style.whiteSpace = "wrap";
+    
+  } 
+} 
+  
