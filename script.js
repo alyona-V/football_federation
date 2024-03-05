debff6b46f856bacc82357e1633db46182a70614
okchichkina <olka.chichkina@gmail.com>
2024-03-06 01:27:27 +0300

    end

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const menuItems = document.querySelector('.menu-items');
+    const page = document.querySelector('.page');
   
     burgerMenu.addEventListener('click', function() {
       menuItems.classList.toggle('show');
+      page.classList.toggle('down');
     });
   });
